c2b96447996bc903ad291f9d65f4011981e7a48b
Kosta <github@ks-baumann.de>
2014-04-27 17:38:23 +0200

    update gcc to 4.9

--- update-gcc-latest.sh
+++ update-gcc-latest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ev
 
-apt-get install -qq gcc-4.8
-apt-get install -qq g++-4.8
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
-update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
+apt-get install -qq gcc-4.9
+apt-get install -qq g++-4.9
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 20
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 20
 update-alternatives --config gcc
 update-alternatives --config g++
 g++ --version
