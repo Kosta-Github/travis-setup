ffbb1e825b1c90eaa7ff3b8b13361ec0527b2582
Kosta <Kosta-Github@users.noreply.github.com>
2014-08-20 15:30:21 +0200

    removed `raring` sources

--- update-environment.sh
+++ update-environment.sh
@@ -3,10 +3,10 @@ set -ev
 
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ saucy main universe"
-add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ raring main universe"
-add-apt-repository -y "deb http://llvm.org/apt/raring/ llvm-toolchain-raring main"
+# add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ raring main universe"
+# add-apt-repository -y "deb http://llvm.org/apt/raring/ llvm-toolchain-raring main"
 
-wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
+# wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
 
 apt-get update -qq
 
