bcdb44935c7bafea05694833bc5292b58e62039a
Kosta <Kosta-Github@users.noreply.github.com>
2015-06-09 10:21:50 +0200

    fix: use `Ubuntu` `precise` packages matching the Travis environment

--- update-environment.sh
+++ update-environment.sh
@@ -2,12 +2,9 @@
 set -ev
 
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
-add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ saucy main universe"
-# add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ raring main universe"
-# add-apt-repository -y "deb http://llvm.org/apt/raring/ llvm-toolchain-raring main"
-
-# wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
+add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ precise main universe"
+add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise main"
 
 apt-get update -qq
-
 apt-get install -qq cmake
+apt-get install clang-3.6 lldb-3.6
