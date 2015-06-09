24c4ce0db2ae4c58e551cb22fbcbec8c9314200c
Kosta <Kosta-Github@users.noreply.github.com>
2015-06-09 10:30:54 +0200

    add `llvm` signatures

--- update-environment.sh
+++ update-environment.sh
@@ -3,7 +3,10 @@ set -ev
 
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ precise main universe"
+
+# setup llvm: http://llvm.org/apt/
 add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise main"
+wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 
 apt-get update -qq
 apt-get install -qq cmake
