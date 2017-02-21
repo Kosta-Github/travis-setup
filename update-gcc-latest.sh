f6e5f39d7820ff39c8ae0b8d6ebb6fbb1e24e9ee
Kosta <Kosta-Github@users.noreply.github.com>
2017-02-21 18:24:37 +0100

    add `-y` flag to `apt-get install` call

--- update-gcc-latest.sh
+++ update-gcc-latest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 
-apt-get install -qq gcc-4.8
-apt-get install -qq g++-4.8
+apt-get install -qq -y gcc-4.8
+apt-get install -qq -y g++-4.8
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
 update-alternatives --config gcc
